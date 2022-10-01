1593d0c926a26726e4a2bbad01a886b6fdd3f265
Brian Michael Poblete <brianmp.dev@gmail.com>
2022-10-01 00:55:23 -0400

    Cleaned up comments and conditions. Added basic error checking.

--- script.js
+++ script.js
@@ -8,34 +8,40 @@ window.onload = addbtn;
 function addbtn() {
     /* Adds button to page */
     /* Initial if statement to check if button already exists */
-    if(document.getElementById("github-dev-btn") == null){
-        let anchor;
-        let elems = document.getElementsByTagName('div');
-        for (let i = 0; i < elems.length; i++) {
-            /**
-             * Targets the div with the branch menu, "Go to file", "Add file", 
-             * and "Code" buttons are
-             */
-            if(elems[i].classList.contains('flex-items-start')){
-                anchor = elems[i];
-            }
+    if(document.getElementById("github-dev-btn") != null)
+        return;
+
+    let anchor;
+    let elems = document.getElementsByTagName('div');
+    for (let i = 0; i < elems.length; i++) {
+        /**
+         * Targets the div with the branch menu, "Go to file", "Add file", 
+         * and "Code" buttons are
+         */
+        if(elems[i].classList.contains('flex-items-start')){
+            anchor = elems[i];
         }
-
-        /* Creates button */
-        const devbtn = document.createElement('a');
-
-        /* Sets button attributes */
-        devbtn.innerHTML = '<a id="github-dev-btn" class="btn ml-2 d-none d-md-block" style="color:#539BF5; background: var(--color-accent-subtle);">GitHub.dev</a>'
-
-        /* Get url */
-        let devurl = window.location.href;
-        // find first occurance of 'com' and replace with 'dev'
-        devurl = devurl.replace(/com/g, 'dev');
-        devbtn.href = devurl;
-
-        // meant to insert after the branch menu
-        if(anchor.firstChild.nextSibling.classList.contains('position-relative'))
-            anchor.insertBefore(devbtn, anchor.firstChild.nextSibling.nextSibling);
     }
+
+    /* Error checking: if anchor is null, don't do anything */
+    if(anchor == null)
+        return;
+
+    /* Creates button */
+    const devbtn = document.createElement('a');
+
+    /* Sets button attributes */
+    devbtn.innerHTML = '<a id="github-dev-btn" class="btn ml-2 d-none d-md-block" style="color:#539BF5; background: var(--color-accent-subtle);">GitHub.dev</a>'
+
+    /* Get url */
+    let devurl = window.location.href;
+    /* find first occurance of 'com' and replace with 'dev' */
+    devurl = devurl.replace(/com/g, 'dev');
+    devbtn.href = devurl;
+
+    /* meant to insert after the branch menu */
+    if(anchor.firstChild.nextSibling.classList.contains('position-relative'))
+        anchor.insertBefore(devbtn, anchor.firstChild.nextSibling.nextSibling);
 }
+
 
